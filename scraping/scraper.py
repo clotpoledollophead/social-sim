ab03ab248fa461c09cde2466ab1e962a848f38b3
luluheart77 <g8057012345111@gmail.com>
2025-08-01 11:51:27 +0800

    new_var

--- scraping/scraper.py
+++ scraping/scraper.py
@@ -45,7 +45,9 @@ for submission in subreddit.top(limit=NUM_POSTS, time_filter='all'):
         'score': submission.score,
         'selftext': submission.selftext,
         'title': submission.title,
-        'url': submission.url
+        'url': submission.url,
+        'upvote_ratio':submission.upvote_ratio,
+        'over_18':submission.over_18
     })
 
     # Scrape top 10 comments for each submission
@@ -62,7 +64,8 @@ for submission in subreddit.top(limit=NUM_POSTS, time_filter='all'):
                 'comment_id': comment.id,
                 'parent_id': comment.parent_id,
                 'score': comment.score,
-                'submission_id': submission.id
+                'submission_id': comment.id,
+                'upvote_ratio':comment.upvote_ratio
             }
             all_comments_data.append(comment_info)
             comments_processed += 1
